39af712f4c0ebb011182f436fb2b4c59fa9e3860
Chun-Yu Lin <chunyu612016@gmail.com>
2023-12-14 21:30:17 +0800

    chage the file loading method - load text file into documents directly

--- summary.py
+++ summary.py
@@ -4,6 +4,7 @@
 
 import getpass
 from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import TextLoader
 from langchain.chains.summarize import load_summarize_chain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.prompts import PromptTemplate
@@ -13,22 +14,23 @@ from langchain.prompts import PromptTemplate
 OPENAI_API_KEY = getpass.getpass(prompt='open api key: ')
 chat = ChatOpenAI(temperature=0.0, model='gpt-3.5-turbo-1106', openai_api_key=OPENAI_API_KEY)
 
+
 # %%
 ### Load and preprocess transcribed text
 ## Read trainscribe text
-#filename = input('輸入文字檔路徑:')
-filename = '魚活通'
+filename = input('輸入文字檔檔名:')
+loader = TextLoader(f'{filename}.txt')
+transcription = loader.load()
 
-with open(f'{filename}.txt') as file:
-    transcription = file.read()
 
+#%%
 ## Split transcribe text into chunks
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size = 1000,
     chunk_overlap = 20
 )
 
-split_texts_list = text_splitter.split_text(transcription)
+split_texts_list = text_splitter.split_text(transcription[0].page_content)
 docs = text_splitter.create_documents(split_texts_list)
 
 # %%
@@ -69,7 +71,7 @@ result = chain({"input_documents": docs}, return_only_outputs=True)
 # %%
 print(result['output_text'])
 
-with open('summary.txt', 'w') as file:
+with open(f'{filename}_summary.txt', 'w') as file:
     file.write(result['output_text'])
 
 
