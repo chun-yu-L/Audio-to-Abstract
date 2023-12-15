7fbcb474e485bfcce1490ddf72a8e9c48bbd55ee
Chun-Yu Lin <chunyu612016@gmail.com>
2023-12-15 15:02:37 +0800

    Modularized code

--- audio_to_text.py
+++ audio_to_text.py
@@ -1,33 +1,31 @@
-# %%
-### install packages
-#pip install -U openai-whisper
-#brew install ffmpeg
-
 import os
 import whisper
 from whisper.utils import get_writer # control output format
 
-# %%
-audiofile = input('輸入音檔路徑:')
 
-# %%
-## 取出音檔名稱作為後續output名稱
-file_name_with_extension = os.path.basename(audiofile)
-filename, extension = os.path.splitext(file_name_with_extension)
+def convert_audio_to_text(audiofile):
+    # 取出音檔名稱作為後續output名稱
+    file_name_with_extension = os.path.basename(audiofile)
+    filename, extension = os.path.splitext(file_name_with_extension)
+    
+    # 載入模型
+    model = whisper.load_model('medium')
+    
+    # 轉錄音檔
+    result = model.transcribe(audiofile, language='zh')
+    
+    ## 儲存 txt 檔
+    txt_writer = get_writer('txt', './')
+    txt_writer(result, filename)
+    
+    #儲存 srt 檔
+    srt_writer = get_writer('srt', './')
+    srt_writer(result, filename)
 
-# %%
-model = whisper.load_model('medium')
-
-# %%
-result = model.transcribe(audiofile, language='zh')
-
-# %%
-## Save text file
-txt_writer = get_writer('txt', './')
-txt_writer(result, filename)
-
-## Save srt file
-srt_writer = get_writer('srt', './')
-srt_writer(result, filename)
+if __name__ == "__main__":
+    # 如果直接執行這個腳本，讓使用者輸入音檔路徑
+    audiofile = input('輸入音檔路徑:')
+    convert_audio_to_text(audiofile)
+    
 
 
--- summary.py
+++ summary.py
@@ -1,7 +1,3 @@
-# %%
-#!pip install openai
-#!pip install --upgrade langchain
-
 import getpass
 from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import TextLoader
@@ -10,68 +6,76 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.prompts import PromptTemplate
 
 
-# %%
-OPENAI_API_KEY = getpass.getpass(prompt='open api key: ')
-chat = ChatOpenAI(temperature=0.0, model='gpt-3.5-turbo-1106', openai_api_key=OPENAI_API_KEY)
+def summarize_text(filename):
+    # 輸入 OpneAI API Key
+    OPENAI_API_KEY = getpass.getpass(prompt='open api key: ')
+    
+    # intialize ChatOpenAI object
+    chat = ChatOpenAI(temperature=0.0, model='gpt-3.5-turbo-1106', openai_api_key=OPENAI_API_KEY)
 
+    # Load transcribed text
+    loader = TextLoader(filename)
+    transcription = loader.load()
 
-# %%
-### Load and preprocess transcribed text
-## Read trainscribe text
-filename = input('輸入文字檔檔名:')
-loader = TextLoader(f'{filename}.txt')
-transcription = loader.load()
+    # Split transcribed text into chunks
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=20
+    )
+    split_texts_list = text_splitter.split_text(transcription[0].page_content)
+    docs = text_splitter.create_documents(split_texts_list)
+    
+    # Define prompt
+    prompt_template = """Write a comprehensive summary of the following text with bullet points:\n
+    {text}\n
+    SUMMARY IN TRADITIONAL CHINESE:"""
+    prompt = PromptTemplate.from_template(prompt_template)
 
+    refine_template = (
+        "Your job is to produce a final summary\n"
+        "We have provided an existing summary up to a certain point: {existing_answer}\n"
+        "We have the opportunity to refine the existing summary"
+        "with some more context below.\n"
+        "------------\n"
+        "{text}\n"
+        "------------\n"
+        "Given the new context, refine the original summary\n"
+        "Generate a clear and concise topic sentence that captures the main theme of the content."
+        "This will be used as the title for the summary."
+        "The output should include a title."
+        "Additionally, generate an abstract section with a one-paragraph summary."
+        "The main point section should comprehensively and clearly describe all key points in the format of bullet points."
+        "\n\nSUMMARY IN TRADITIONAL CHINESE:"
+    )
+    refine_prompt = PromptTemplate.from_template(refine_template)
 
-#%%
-## Split transcribe text into chunks
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size = 1000,
-    chunk_overlap = 20
-)
+    # 載入並執行 summarization chain
+    chain = load_summarize_chain(
+        llm=chat,
+        chain_type="refine",
+        question_prompt=prompt,
+        refine_prompt=refine_prompt,
+        return_intermediate_steps=True,
+        input_key="input_documents",
+        output_key="output_text",
+    )
+    result = chain({"input_documents": docs}, return_only_outputs=True)
 
-split_texts_list = text_splitter.split_text(transcription[0].page_content)
-docs = text_splitter.create_documents(split_texts_list)
+    # 回傳結果
+    return result
 
-# %%
-### Prompt engineering
-prompt_template = """Write a comprehensive summary of the following text with bullet points:\n
-{text}\n
-SUMMARY IN TRADITIONAL CHINESE:"""
-prompt = PromptTemplate.from_template(prompt_template)
+if __name__ == "__main__":
+    # 讓使用者輸入文字檔檔名
+    filename = input('輸入文字檔路徑:')
 
-refine_template = (
-    "Your job is to produce a final summary\n"
-    "We have provided an existing summary up to a certain point: {existing_answer}\n"
-    "We have the opportunity to refine the existing summary"
-    "with some more context below.\n"
-    "------------\n"
-    "{text}\n"
-    "------------\n"
-    "Given the new context, refine the original summary\n"
-    "Generate a clear and concise topic sentence that captures the main theme of the content."
-    "This will be used as the title for the summary."
-    "The output should include a title."
-    "Additionally, generate an abstract section with a one-paragraph summary."
-    "The main point section should comprehensively and clearly describe all key points in the format of bullet points."
-    "\n\nSUMMARY IN TRADITIONAL CHINESE:"
-)
-refine_prompt = PromptTemplate.from_template(refine_template)
-chain = load_summarize_chain(
-    llm=chat,
-    chain_type="refine",
-    question_prompt=prompt,
-    refine_prompt=refine_prompt,
-    return_intermediate_steps=True,
-    input_key="input_documents",
-    output_key="output_text",
-)
-result = chain({"input_documents": docs}, return_only_outputs=True)
+    # 生成摘要
+    summary_result = summarize_text(filename)
 
-# %%
-print(result['output_text'])
+    # 印出摘要結果
+    print(summary_result['output_text'])
 
-with open(f'{filename}_summary.txt', 'w') as file:
-    file.write(result['output_text'])
+    # 儲存摘要結果
+    with open(f'{filename}_summary.txt', 'w') as file:
+        file.write(summary_result['output_text'])
 
 
